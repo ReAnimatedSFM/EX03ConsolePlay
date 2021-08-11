764ee350b7dde8ca9695236c81eb48d621ae1655
William Johannes Saaby Porsmose <will5606@edu.campusvejle.dk>
2021-08-11 09:43:46 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace EX03ConsolePlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console layout
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WindowWidth = 120;
+            Console.WindowHeight = 30;
+
+            //Print
+            Console.WriteLine();
+            Console.WriteLine("Hvid, grøn");
+
+            //Console color change
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            //Print
+            Console.WriteLine();
+            Console.WriteLine("Blå, gul");
+
+            //beep
+            Console.Beep(4000, 1000);
+            Console.Beep(2000, 1500);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
